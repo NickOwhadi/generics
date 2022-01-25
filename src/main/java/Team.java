1d6a6ffbd44cc90026d4f5a2c16850d2d10409f9
n7owhad <n7owhad@hotmail.com>
2022-01-25 01:26:51 -0500

    Initial commit

---
+++ src/main/java/Team.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Team<T extends Player> {
+
+    private String name;
+
+    private ArrayList<T> members = new ArrayList<>();
+
+    public Team(String name) {
+
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean addPlayer(T player) {
+
+        if (members.contains(player)) {
+            System.out.println(player.getName() + " is already on this team");
+            return false;
+        } else {
+            members.add(player);
+            System.out.println((getName()) + " is picked for this ream" + this.name);
+            return true;
+        }
+    }
+    public int numPlayers(){
+        return this.members.size();
+    }
+}
+
